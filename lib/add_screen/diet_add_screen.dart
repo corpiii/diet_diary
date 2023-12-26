38d7cd0d15afcede78c89bdf981c9cfd965ffddf
corpi <lj845854@gmail.com>
2023-12-26 16:57:11 +0900

    feat: imagePicker를 통한 이미지 삽입

--- lib/add_screen/diet_add_screen.dart
+++ lib/add_screen/diet_add_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class DietAddScreen extends StatefulWidget {
   const DietAddScreen({super.key});
@@ -8,63 +11,86 @@ class DietAddScreen extends StatefulWidget {
 }
 
 class _DietAddScreenState extends State<DietAddScreen> {
+  ImagePicker imagePicker = ImagePicker();
+  bool isExistImage = false;
+  XFile? _selectedImage;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const CloseButton(
-          color: Colors.blue,
-        ),
-        title: const Text('추가'),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              '완료',
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          leading: const CloseButton(
+            color: Colors.blue,
+          ),
+          title: const Text('추가'),
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                '완료',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            titleTextFieldWidget(),
-            // imagePickerWidget(),
-            // bodyTextFieldWidget(),
+            )
           ],
         ),
-      )
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              titleTextFieldWidget(),
+              imagePickerWidget(),
+              // bodyTextFieldWidget(),
+            ],
+          ),
+        ));
   }
 
   Widget titleTextFieldWidget() {
-    return Expanded(
-      child: TextField(
-        autofocus: true,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
+    return TextField(
+      autofocus: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(20),
-            borderSide: BorderSide(
-              color: Colors.blue,
-              width: 1.0
-            )
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.blue,
-              width: 1.0
-            )
-          ),
-          hintText: '00/00 00시',
-        ),
-      )
+            borderSide: const BorderSide(color: Colors.blue, width: 1.0)),
+        enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue, width: 1.0)),
+        hintText: '00/00 00시',
+      ),
     );
+  }
+
+  Widget imagePickerWidget() {
+    return GestureDetector(
+      onTap: () {
+        getImage();
+      },
+      child: _selectedImage == null
+          ? Container(
+              width: 250,
+              height: 250,
+              color: Colors.grey,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset(
+                  'assets/image_add_icon.png',
+                ),
+              ),
+            )
+          : Image.file(
+              File(_selectedImage!.path),
+              width: 250,
+            ),
+    );
+  }
+
+  Future<void> getImage() async {
+    _selectedImage = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {});
   }
 }
