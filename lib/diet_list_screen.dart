3dc0b60313953fa25fa41d586bb4c6e65eb4206b
corpi <lj845854@gmail.com>
2023-12-26 14:39:18 +0900

    feat: 앱바 내 추가 액션 버튼 위젯 생성

--- lib/diet_list_screen.dart
+++ lib/diet_list_screen.dart
@@ -6,42 +6,55 @@ class DietListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('식단 리스트'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8.0),
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.deepPurple,
-                width: 200,
-                height: 70,
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.deepPurple,
-                width: 200,
-                height: 70,
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.deepPurple,
-                width: 200,
-                height: 70,
-              ),
-            ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('식단 리스트'),
+          actions: [
+            iconButtonWidget(onPressed: () {}),
+            const SizedBox(width: 10,)
           ],
         ),
-      )
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.deepPurple,
+                  width: 200,
+                  height: 70,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.deepPurple,
+                  width: 200,
+                  height: 70,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.deepPurple,
+                  width: 200,
+                  height: 70,
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Widget iconButtonWidget({required void Function() onPressed}) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: const Icon(
+        Icons.add,
+        color: Colors.blue,
+        size: 30,
+      ),
     );
   }
 }
